a5ed35d4534373d18ccd07a908b8de503d191f2d
Thiyaneshwaran <52602144+th4nu-0x0@users.noreply.github.com>
2020-11-08 20:53:22 +0530

    Update js.js

--- assets/js.js
+++ assets/js.js
@@ -1,5 +1,5 @@
 const uname = document.querySelector("span.navbar-text strong").innerHTML;
-const editUname = uname.slice(0, -1)
+const editUname = uname.slice(0, -1);
 var req = new XMLHttpRequest();
 req.open('get',`http://barker-social.com:32880/profile/${editUname}/edit/`,true);
 req.send();
